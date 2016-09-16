ea59e1283535da5abbc86814402887fd22d79376
jibraj neupane <jibraj neupane>
2016-09-16 10:12:06 +0545

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,22 @@
+import javax.swing.JOptionPane;
+
+/**
+ * @author user
+ *
+ */
+/**
+ * @author user
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+	
+
+		System.out.println("hello world");
+		System.out.println("I love programming");
+		JOptionPane.showMessageDialog(null, "hello world");
+
+	}
+
+}
